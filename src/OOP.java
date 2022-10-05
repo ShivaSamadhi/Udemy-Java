9f0ab49804ffc689d4d9410e1107470fd075cf11
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-05 12:45:45 -0500

    udemy practice

---
+++ src/OOP.java
@@ -0,0 +1,15 @@
+package src;
+
+public class OOP {
+    //ENCAPSULATION
+    //ABSTRACTION
+    //INHERITANCE
+    //POLYMORPHISM
+
+    //OBJECTS STORE THEIR STATES IN FIELDS (AKA VARIABLES) & EXPOSE THEIR BEHAVIORS VIA METHODS (AKA FUNCTIONS)
+
+
+    public static void main(String[] args) {
+
+    }
+}
