6a17789ad0ae935c6296af3326395352d6b4da3b
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-07 12:22:08 -0500

    fish class created

--- src/Fish.java
+++ src/Fish.java
@@ -1,4 +1,26 @@
 package src;
 
-public class Fish {
+public class Fish extends Animal {
+    private int gills;
+    private int eyes;
+    private int fins;
+
+    public Fish(String name, int size, int weight, int gills, int eyes, int fins) {
+        super(name, 1, 1, size, weight);
+        this.gills = gills;
+        this.eyes = eyes;
+        this.fins = fins;
+    }
+
+    public int getGills() {
+        return gills;
+    }
+
+    public int getEyes() {
+        return eyes;
+    }
+
+    public int getFins() {
+        return fins;
+    }
 }
