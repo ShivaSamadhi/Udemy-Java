7cd439c34199852f617d38ffe25f5690c78f7f76
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-10 13:31:23 -0500

    resolution class added

--- src/Moniter.java
+++ src/Moniter.java
@@ -1,4 +1,34 @@
 package src;
 
 public class Moniter {
+    private String model;
+    private String manufacturer;
+    private int size;
+    private Resolution nativeResolution;
+
+    public Moniter(String model, String manufacturer, int size, Resolution nativeResolution) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.size = size;
+        this.nativeResolution = nativeResolution;
+    }
+    public void drawPixelAt(int x, int y, String color){
+        System.out.printf("Drawing pixel at (%s,%s) in %s", x, y , color);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Resolution getNativeResolution() {
+        return nativeResolution;
+    }
 }
