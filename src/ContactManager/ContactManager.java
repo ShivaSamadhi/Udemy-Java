7409981799f69a2effa18f7cde2b5cbcc28be64a
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-11 15:07:59 -0500

    contact manager ready for testing

---
+++ src/ContactManager/ContactManager.java
@@ -0,0 +1,4 @@
+package src.ContactManager;
+
+public class ContactManager {
+}
--- src/ContactManager/MobilePhone.java
+++ src/ContactManager/MobilePhone.java
@@ -1,4 +1,84 @@
 package src.ContactManager;
 
+import java.util.ArrayList;
+
 public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+    
+    public boolean addNewContact(Contact contact){
+        if (findContact(contact.getName()) >= 0) {
+            System.out.println("Contact already exists");
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
+    public boolean updateContact(Contact oldContact, Contact newContact){
+        int foundPosition = findContact(oldContact);
+        if(foundPosition < 0){
+            System.out.printf("%s was not found", oldContact.getName());
+            return false;
+        }
+        this.myContacts.set(foundPosition, newContact);
+        System.out.printf("%s was updated to %s", oldContact.getName(), newContact.getName());
+        return true;
+    }
+    private int findContact(Contact contact){
+        return this.myContacts.indexOf(contact);
+    }
+    private int findContact(String contactName){
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            Contact contact = this.myContacts.get(i);
+            if (contact.getName().equals(contactName)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public String queryContact (Contact contact){
+        if (findContact(contact) >= 0){
+            return contact.getName();
+        }
+        return null;
+    }
+
+    public boolean removeContact (Contact contact){
+        int foundPosition = findContact(contact);
+        if(foundPosition < 0){
+            System.out.printf("%s was not found", contact.getName());
+            return false;
+        }
+        this.myContacts.remove(foundPosition);
+        System.out.printf("%s was deleted", contact.getName());
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
